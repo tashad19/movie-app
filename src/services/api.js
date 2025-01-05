563957ced1cb34316069216fcca3764e58aed7b1
tashad19 <tashadurrahman1924@gmail.com>
2025-01-05 23:57:24 +0530

    fixed api stuff

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,9 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = "https://api.themoviedb.org/3";
-
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-  const data = await response.json();
-  return data.results;
+  const response = await fetch("https://movie-app-backend-62gi.onrender.com/api/popular-movies");
+  return await response.json();
 };
 
 export const searchMovies = async (query) => {
-  const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
-      query
-    )}`
-  );
-  const data = await response.json();
-  return data.results;
+  const response = await fetch(`https://movie-app-backend-62gi.onrender.com/api/search-movies?query=${encodeURIComponent(query)}`);
+  return await response.json();
 };
