cb57021f579736998e7d0e151cfb21b035357ee3
tashad19 <tashadurrahman1924@gmail.com>
2025-01-05 23:17:16 +0530

    add initial files

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import "./css/index.css";
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+)
